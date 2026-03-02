37bad62961e48502818b21754496c0edcc7422a8
SrimanKumar238 <borrasriman@gmail.com>
2026-03-02 16:27:28 +0530

    UC7: Deque-based optimized palindrome checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,41 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Palindrome Checker App");
 
-        // Hardcoded word
-        String word = "madam";
+        Scanner scanner = new Scanner(System.in);
 
-        String reversed = new StringBuilder(word).reverse().toString();
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine().toLowerCase();
 
-        if (word.equalsIgnoreCase(reversed)) {
+        Deque<Character> deque = new LinkedList<>();
+
+        // Add all characters to deque
+        for (char ch : word.toCharArray()) {
+            deque.addLast(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and back
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is NOT a palindrome.");
         }
+
+        scanner.close();
     }
 }
